af03ed87afc58c49600f95e2e4bae3f9ccdcb1bc
Kumar Shubham <kumar.shubham2015@vit.ac.in>
2016-09-29 01:36:27 +0530

    Added all the main code, tests left

--- client/main.js
+++ client/main.js
@@ -1,36 +1,51 @@
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import { ReactiveDict } from 'meteor/reactive-dict';
 import { Mongo } from 'meteor/mongo';
 import './main.html';
 
 //getting the comments collection for all the collections
 const Comments = new Mongo.Collection('comments');
 
-Template.body.helpers({
-    showComments() {
-        return Comments.find({},{sort:{createdAt: -1}});
-    }
+Template.body.onCreated(()=>{
+  this.state = new ReactiveDict();
+  Meteor.subscribe('comments');
 });
+
+Template.body.helpers({
+    ShowComments(){
+    return Comments.find({}, { sort: { createdAt: -1 } });
+  },
+  isOwner(){
+      return this;
+  }
+});
+
+
+Template.registerHelper('log',(what)=>{
+    console.log(what);
+    //console.log(Meteor.userId());
+});
+
 Template.body.events({
     'submit .new-comment'(event) {
         event.preventDefault();
+        //console.log(Meteor.user());
         const target = event.target;
         const text = target.comment.value;
-        Comments.insert({
-            text: text,
-            ownerid: Meteor.userId(),
-            email: Meteor.user().emails[0].address,
-            createdAt: new Date()
-        });
+        console.log(text);
+        Meteor.call('comments.insert',text);
         target.comment.value = '';
     },
-    'click .delete'(event){
-        var conf = confirm("Are you sure you want to delete it?");
-        if(conf){
-            Comments.remove(this._id);
-        }
-        else{
-            return false;
-        }
+    'click .logout'(){
+        window.localStorage.removeItem('Meteor.loginToken');
+        window.location = '/';
+    },
+    'click .delete'(){
+        Meteor.call('comments.delete',this._id,function(err,result){
+            console.log(err);
+            if(err.error === 'not-authorized'){
+                alert("Sorry! You can't delete someone else's comment");
+            }            
+        });
     }
 });
--- server/main.js
+++ server/main.js
@@ -1,6 +1,37 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check';
 const Comments = new Mongo.Collection('comments');
-Meteor.startup(() => {
-  // code to run on server at startup
+
+if (Meteor.isServer) {
+	Meteor.publish('comments', () => {
+		return Comments.find({});
+	});
+}
+
+
+Meteor.methods({
+	'comments.insert'(todo) {
+		check(todo, String);
+		//in order to make sure that the user is loggedIn
+		if (!this.userId) {
+			throw new Meteor.Error('Unauthorized');
+		}
+		Comments.insert({
+			text: todo,
+			owner: this.userId,
+			email: Meteor.users.findOne(this.userId).username,
+			createdAt: new Date()
+		});
+	},
+	'comments.delete'(commentId) {
+		check(commentId, String);
+		
+		const comment = Comments.findOne(commentId);
+		
+		if (comment.owner !== Meteor.user()._id) {
+			throw new Meteor.Error('not-authorized');
+		}
+		Comments.remove(comment);
+	}
 });
